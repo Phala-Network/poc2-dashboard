ee3815eccf3451c7b477a2139fde61b42d77de8f
h4x3rotab <h4x3rotab@gmail.com>
2020-07-20 18:53:12 +0800

    UI new design

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app" class="app">
     <div class="header">
-      <div class="header-wrap">
-        <router-link class="header-logo-link" to="/">
-          <img class="header-logo" src="@/assets/img/logo.png" alt="PHALA LOGO">
-        </router-link>
-        <div class="header-title">Phala.Network</div>
-        <div class="header-sub-title">Blockchain Confidentiality by Trusted Computing</div>
+      <div class="title">
+        Phala Network PoC-2 Dashboard
+      </div>
+      <div class="links">
+        <a href="https://phala.network">Home Page</a>
+        -
+        <a href="https://app.phala.network">Testnet Web Apps</a>
+        -
+        <a href="https://github.com/Phala-Network">Github</a>
+        -
+        <a href="https://t.me/phalanetwork">Telegram</a>
       </div>
     </div>
 
@@ -15,7 +20,7 @@
     </div>
 
     <div class="footer">
-      <div class="footer-copyright">COPYRIGHT © 2020 PHALA.LTD ALL RIGHTS RESERVED. - Made with love by Phala.LTD</div>
+      <div class="footer-copyright">COPYRIGHT © 2020 PHALA NETWORK ALL RIGHTS RESERVED. - Made with love by Phala Network</div>
     </div>
   </div>
 </template>
@@ -29,18 +34,9 @@ export default {
 <style>
 </style>
 <style lang="scss">
-::-webkit-scrollbar {
-    width: 6px;
-    background: none;
-}
-
-::-webkit-scrollbar-thumb {
-    border-radius:5px;
-    background-color: #02681b;
-}
-
 body {
-    font-family: "Exo 2", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "PingFang", "Droid Sans", "Helvetica Neue", sans-serif;
+    font-size: 12px;
+    font-family: monospace;
     font-weight: 500;
     color: rgb(13, 143, 52);
 }
@@ -56,67 +52,35 @@ html, body {
   background-size: cover;
   .container {
     display: flex;
-    width: 100%;
-    max-width: 1280px;
-    margin: 0 auto 20px;
-    padding-top: 100px;
-    min-height: calc(100vh - 200px);
-    circle {
-      -webkit-transition: stroke-dasharray .25s;
-      transition: stroke-dasharray .25s;
-    }
+    max-width: 1200px;
+    margin: 0 auto;
   }
 }
 
 .header {
-  position: fixed;
-  height: 120px;
-  right: 0;
-  left: 0;
-  z-index: 9;
-  transition: all .3s;
-  .header-wrap {
-    width: 1200px;
-    height: 100%;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  padding: 20px;
+    text-align: center;
+    color: #ddd;
+    font-family: monospace;
+
+  .title {
+    font-size: 24px;
   }
-}
 
-.header-logo {
-  margin-left: 0px;
-  transition: width .3s;
-  width: 80px;
-  height: 80px;
-}
-
-.header-logo-link {
-  display: block;
-  outline: none;
-
-  &:hover {
-    cursor: pointer;
+  .links {
+    margin-top: 10px;
+    color: rgb(4, 170, 45);
+    a {
+      color: rgb(4, 170, 45);
+    }
   }
-}
-
-.header-title {
-  font-size: 48px;
-  color:rgb(230, 239, 240);
-}
-
-.header-sub-title {
-  font-size: 24px;
 }
 
 .footer {
   width: 100%;
-  height: 80px;
   box-sizing: border-box;
   margin-top: 20px;
   padding: 15px 0;
-  background:linear-gradient(180deg, #050308 0%,rgb(34, 36, 36) 100%);
   border-top: 3px solid #1f5f35;
   text-align: center;
 }
--- app/src/components/Dashboard.vue
+++ app/src/components/Dashboard.vue
@@ -1,44 +1,52 @@
 <template>
   <div class="dashboard">
     <section class="section-data">
-      <div class="th">
-        <span class="th-tee"><input type="checkbox" v-model="disp_tee" @click="set_tee">only tee</span>
-        <span class="th-gatekeeper"><input type="checkbox" v-model="disp_gatekeeper" @click="set_gatekeeper">only gatekeeper</span>
-        <span class="th-status"><input type="checkbox" v-model="disp_online" @click="set_online">only online</span>
-        <span class="th-status"><input type="checkbox" v-model="disp_offline" @click="set_offline">only offline</span>
-      </div>
-      <div class="data-header">
-        <div class="data-tab tab-status">Status</div>
-        <div class="data-tab tab-role">Role</div>
-        <div class="data-tab tab-name ">Node Name</div>
-        <div class="data-tab tab-account">Phala Address</div>
-        <div class="data-tab tab-city">City</div>
-        <div class="data-tab tab-era">Online Eras</div>
-        <div class="data-tab tab-era">Gatekeeper Eras</div>
-        <div class="data-tab tab-era">Slashed Eras</div>
-        <div class="data-tab tab-time">Connect at</div>
+      <div class="checks">
+        <span><input type="checkbox" v-model="disp_tee" @click="set_tee">TEE Only</span>
+        <span><input type="checkbox" v-model="disp_gatekeeper" @click="set_gatekeeper">Gatekeeper Only</span>
+        <span><input type="checkbox" v-model="disp_online" @click="set_online">Online Only</span>
+        <span><input type="checkbox" v-model="disp_offline" @click="set_offline">Offline Only</span>
       </div>
 
-      <div class="data-table">
-        <div class="tbody">
-          <div v-for="item in filteredData" v-bind:key="item.id" class="tr">
-            <div class="td td-status" v-bind:class="{ 'td td-status-offline': item.online === 0 || item.online == null, 'td-v td-status': item.online === 1 && item.id == null }">{{ item.online === 1 ? "Online":"Offline" }}</div>
-            <div class="td td-role" v-bind:class="{ 'td-v td-role': item.id == null }">
-              <span v-if="item.is_gatekeeper">{{ "Gk" }}</span>
+      <table>
+        <thead>
+          <tr>
+            <th class="data-tab tab-status">Status</th>
+            <th class="data-tab tab-role">Role</th>
+            <th class="data-tab tab-name ">Node</th>
+            <th class="data-tab tab-account">Addr</th>
+            <th v-if="false" class="data-tab tab-city">City</th>
+            <th class="data-tab tab-era">Online Eras</th>
+            <th class="data-tab tab-era">Gatekeeper Eras</th>
+            <th class="data-tab tab-era">Slashed Eras</th>
+            <th v-if="false" class="data-tab tab-time">Connect at</th>
+            <th>Points Est</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in filteredData" v-bind:key="item.id" class="tr">
+            <td class="td-status" v-bind:class="{ 'text-alert': item.online === 0 || item.online == null, 'text-hilight td-status': item.online === 1 && item.id == null }">
+              {{ item.online === 1 ? "Online":"Offline" }}
+            </td>
+            <td class="td-role" v-bind:class="{ 'text-hilight td-role': item.id == null }">
+              <span v-if="item.is_gatekeeper">{{ "GK" }}</span>
               <span v-else-if="item.is_tee && !item.is_gatekeeper">{{ "TEE" }}</span>
               <span v-else-if="item.id != null">{{ "Node" }}</span>
               <span v-else>{{ "" }}</span>
-            </div>
-            <div class="td td-name" v-bind:class="{ 'td-v td-name': item.id == null }">{{ item.node_name }}</div>
-            <div class="td td-account" v-bind:class="{ 'td-v td-account-v': item.id == null }">{{ item.controller?item.controller:"" }}</div>
-            <div class="td td-city" v-bind:class="{ 'td-v td-city': item.id == null }">{{ item.city }}</div>
-            <div class="td td-num" v-bind:class="{ 'td-v td-num': item.id == null }">{{ item.node_eras>=0?item.node_eras:"" }}</div>
-            <div class="td td-num" v-bind:class="{ 'td-v td-num': item.id == null }">{{ item.gatekeeper_eras?item.gatekeeper_eras:0 }}</div>
-            <div class="td td-num" v-bind:class="{ 'td-v td-num': item.id == null, 'td td-num-alert': item.slash_eras > 0 }">{{ item.slash_eras?item.slash_eras:0 }}</div>
-            <div class="td td-timestamp" v-bind:class="{ 'td-v td-timestamp': item.id == null }">{{ get_date_str(item.timestamp) }}</div>
-          </div>
-        </div>
-      </div>
+            </td>
+            <td class="td-name" v-bind:class="{ 'text-hilight td-name': item.id == null }">{{ name_components(item)[0] }}</td>
+            <td class="td-account" v-bind:class="{ 'text-hilight': item.id == null || !!item.controller }">
+              {{ item.controller ? item.controller : name_components(item)[1] }}
+            </td>
+            <td class="td-city" v-if="false" v-bind:class="{ 'text-hilight td-city': item.id == null }">{{ item.city }}</td>
+            <td class="text-center" v-bind:class="{ 'text-hilight td-num': item.id == null }">{{ item.node_eras>=0?item.node_eras:"" }}</td>
+            <td class="text-center" v-bind:class="{ 'text-hilight td-num': item.id == null }">{{ item.gatekeeper_eras?item.gatekeeper_eras:0 }}</td>
+            <td class="text-center" v-bind:class="{ 'text-hilight td-num': item.id == null, 'text-alert': item.slash_eras > 0 }">{{ item.slash_eras?item.slash_eras:0 }}</td>
+            <td v-if="false" class="td-timestamp" v-bind:class="{ 'text-hilight td-timestamp': item.id == null }">{{ get_date_str(item.timestamp) }}</td>
+            <td class="text-center">-</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   </div>
 </template>
@@ -153,12 +161,26 @@ export default {
       }
 
       return new Date(timestamp * 1000).toLocaleString('en-US')
+    },
+
+    name_components(item) {
+      if (!item.node_name) {
+        return ['', '']
+      }
+      const components = item.node_name.split('|').filter(x => !!x.trim());
+      if (components.length < 1) {
+        return ['', '']
+      } else if (components.length == 1) {
+        return [components[0], '']
+      } else {
+        return [components[0], components[1]]
+      }
     }
   },
 
   mounted () {
     const that = this
-    const test = false
+    const test = true
     const fetchData = function () {
       if (!test) {
         const now = new Date().getTime() / 1000
@@ -203,227 +225,68 @@ export default {
 }
 </script>
 <style scoped>
-::-webkit-scrollbar{
-  width: 5px;
-  background: none;
-}
 
-::-webkit-scrollbar-thumb{
-  border-radius:5px;
-  background-color: rgb(4, 170, 45);
-}
 </style>
 <style lang="scss" scoped>
 .dashboard {
-  max-width: 1280px;
+  font-size: 12px;
+  font-family: 'Courier New', Courier, monospace;
+  width: 100%;
+
   .section-data {
     width: 100%;
+    padding: 0 20px;
     min-height: 750px;
     box-sizing: border-box;
     margin-top: 10px;
     margin-bottom: 30px;
     float: left;
     background: rgba(53, 53, 56, 0.75);
-    box-shadow: 0px 2px 6px 0px rgba(19, 13, 51, 0.5);
-    border-radius: 8px;
-    border: 1px solid rgba(80, 70, 128, 0.5);
-
-    .th {
+    .checks {
       width: 100%;
       display: flex;
       height: 40px;
       line-height: 40px;
-      background-color: #b48503;
-      color: #f9fafa;
-      border-radius: 8px 8px 0 0;
-    }
-
-    .th-tee {
-      width: 100px;
-    }
-
-    .th-gatekeeper {
-      width: 150px;
-    }
-
-    .th-status {
-      width: 120px;
-    }
-
-    .data-header {
-      position: relative;
-      display: flex;
-      height: 50px;
-      line-height: 48px;
-      border-bottom: 1px solid rgba(207, 206, 214, 0.5);
-      background-repeat: no-repeat;
-      background-position: -11px -11px;
-      background-size: 346px 20px;
-      background:linear-gradient(180deg,rgb(2, 109, 47) 0%,rgb(4, 170, 45) 100%);
-      .data-tab {
-        font-size: 13px;
-        font-weight: 400;
-        color: rgb(215, 214, 221);
-        text-align: center;
-      }
-
-      .tab-status {
-        width: 60px;
-      }
-
-      .tab-role {
-        width: 50px;
-      }
-
-      .tab-name {
-        width: 280px;
-      }
-
-      .tab-account {
-        width: 200px;
-      }
-
-      .tab-era {
-        width: 115px;
-        word-wrap: false;
-      }
-
-      .tab-city {
-        width: 140px;
-      }
-
-      .tab-time {
-        width: 180px;
-      }
-
-      .tab-rewards {
-        width: 90px;
+      color: rgb(4, 170, 45);
+      border-bottom: solid #0f0 1px;
+      margin-bottom: 10px;
+      span {
+        margin: 0 10px;
       }
     }
+    table {
+      width: 100%;
 
-    .data-table {
-      font-size: 14px;
-      font-weight: 500;
-
-      .tbody {
-        height: 700px;
-        margin-right: 3px;
-        overflow-y: scroll;
-      }
-
-      .tr {
-        width: 100%;
-        display: flex;
-      }
-
-      .tr {
-        position: relative;
-        height: 32px;
-        line-height: 27px;
-        background-color: #656468;
-
-        &:nth-child(odd) {
-          background-color: rgba(53, 53, 56, 0.75);
-        }
-
-        .td {
-          flex: 1;
-          color: #f9fafa;
-          text-align: left;
-        }
-
-        .td-v {
-          flex: 1;
-          color: #54eb0e;
-          text-align: left;
-        }
-
-        .td-status {
-          width: 60px;
-          flex: none;
-        }
-
-        .td-status-offline {
-          width: 60px;
-          flex: none;
-          color: rgb(250, 20, 20);
-        }
-
-        .td-role {
-          width: 50px;
-          flex: none;
-        }
-
-        .td-name {
-          overflow: hidden;
-          width: 280px;
-          margin-left: 10px;
-          flex: none;
-          white-space: nowrap;
-        }
-
-        .td-account {
-          overflow: hidden;
-          width: 200px;
-          margin-left: 10px;
-          flex: none;
-          color: rgb(129, 220, 243);
-          text-decoration: none;
-          &:hover {
-            cursor: pointer;
-          }
-        }
-
-        .td-account-v {
-          overflow: hidden;
-          width: 200px;
-          margin-left: 10px;
-          flex: none;
-          color: #54eb0e;
-          text-decoration: none;
-          &:hover {
-            cursor: pointer;
-          }
-        }
-
-        .td-city {
-          overflow: hidden;
-          width: 150px;
-          margin-left: 10px;
-          flex: none;
-        }
-
-        .td-num {
-          text-align: center;
-          width:105px;
-          flex: none;
-        }
-
-        .td-num-alert {
-          text-align: center;
-          width:105px;
-          flex: none;
-          color: rgb(255, 20, 20);
-        }
-
-        .td-timestamp {
-          width:180px;
-          flex: none;
-        }
-
-        .td-rewards {
-          text-align: left;
-          width:80px;
-          flex: none;
-        }
-      }
-
-      .td {
-        flex: 1;
-        color: #f9fafa;
-        text-align: left;
-      }
+      td:nth-child(1) { width: 10%; }
+      td:nth-child(2) { width: 5%; }
+      td:nth-child(3) { width: 20%; }
+      td:nth-child(4) { width: 40%; }
+      td:nth-child(5) { width: 5%; }
+      td:nth-child(6) { width: 5%; }
+      td:nth-child(7) { width: 5%; }
+      td:nth-child(8) { width: 5%; }
+    }
+    thead {
+      color: #0f0;
+    }
+    tbody td {
+      text-overflow: ellipsis;
+      overflow: hidden;
+      word-wrap: break-word;
     }
   }
 }
+
+.text-center {
+  text-align: center;
+}
+
+.text-alert {
+  color: rgb(255, 20, 20);
+}
+
+.text-hilight {
+  color: #54eb0e;
+}
+
 </style>
